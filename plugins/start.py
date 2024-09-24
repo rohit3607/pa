6dbb601c4b87349aed3d4984d852942b112a8151
rohit870891 <koyeb77user@gmail.com>
2024-09-25 02:53:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -39,7 +39,7 @@ async def start_command(client: Client, message: Message):
     text = message.text
     is_premium = await is_premium_user(id)
     verify_status = await get_verify_status(id)
-            if verify_status['is_verified'] and VERIFY_EXPIRE < (time.time() - verify_status['verified_time']):
+    if verify_status['is_verified'] and VERIFY_EXPIRE < (time.time() - verify_status['verified_time']):
                 await update_verify_status(id, is_verified=False)
             if "verify_" in message.text:
                 _, token = message.text.split("_", 1)
