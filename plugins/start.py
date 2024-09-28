6a6007bb2e59154533ca118dd521184fe17b403e
rohit870891 <koyeb77user@gmail.com>
2024-09-28 19:58:57 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -132,7 +132,7 @@ async def start_command(client: Client, message: Message):
 
         elif string.startswith("get"):
             if not is_premium:
-                if not verify_status['not_verified']:
+                if verify_status['is_verified']:
                     token = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
                     await update_verify_status(id, verify_token=token, link="")
                     link = await  get_shortlink(SHORTLINK_URL, SHORTLINK_API, f'https://telegram.dog/{client.username}?start=verify_{token}')
