6523e3b48490d3a87ed550d731e84f7fcb11623a
rohit870891 <koyeb77user@gmail.com>
2024-09-28 18:25:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -134,8 +134,8 @@ async def start_command(client: Client, message: Message):
             if not is_premium:
                 if not verify_status['is_verified']:
                     token = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
-            await update_verify_status(id, verify_token=token, link="")
-                link = await  get_shortlink(SHORTLINK_URL, SHORTLINK_API, f'https://telegram.dog/{client.username}?start=verify_{token}')
+                    await update_verify_status(id, verify_token=token, link="")
+                    link = await  get_shortlink(SHORTLINK_URL, SHORTLINK_API, f'https://telegram.dog/{client.username}?start=verify_{token}')
                     btn = [
                         [InlineKeyboardButton("Click here", url=link), InlineKeyboardButton('How to use the bot', url=TUT_VID)],  # First row with two buttons
                         [InlineKeyboardButton('BUY PREMIUM', callback_data='buy_prem')]  # Second row with one button
