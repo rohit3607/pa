a47d04cfe60fc0410823a63fa185f434921d500f
rohit870891 <koyeb77user@gmail.com>
2024-10-06 09:41:21 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -106,7 +106,6 @@ async def get_verify_status(user_id):
     verify = await db_verify_status(user_id)
     return verify
 
-import time
 
 async def update_verify_status(user_id, verify_token="", is_verified=False, verified_time=time.time(), link=""):
     current = await db_verify_status(user_id)
