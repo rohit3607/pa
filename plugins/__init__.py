4e40228c7401e1f68c3dba702a53f9f64bf24967
rohit3607 <rohitplayer87089@gmail.com>
2024-10-18 00:13:36 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -8,7 +8,21 @@
 from aiohttp import web
 from .route import routes
 
+from enum import Enum
+from collections import defaultdict
 
+class ListenerTypes(Enum):
+    MESSAGE = 'message'
+    # Add other listener types as needed
+
+class YourBotClass:
+    def __init__(self):
+        # Other initialization code...
+        self.listeners = defaultdict(list)
+        # or
+        # self.listeners = {listener_type: [] for listener_type in ListenerTypes}
+
+    # Rest of your class methods...
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
