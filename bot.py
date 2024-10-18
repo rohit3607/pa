b1369e609eebc9d576c37a02d03bc87f0abde891
rohit3607 <rohitplayer87089@gmail.com>
2024-10-18 23:24:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ from aiohttp import web
 from plugins import web_server
 from pyrogram import Client
 from pyromod import listen
-from pyromod.listen import Listener
 from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
